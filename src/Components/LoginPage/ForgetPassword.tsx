6b3a6f16bd6e8caf2e4ce4d585937a3a3036fdd4
ajayeeposit <aranabhat@eposit.com>
2023-05-25 16:00:24 +0545

    feat:added forgetpassword

---
+++ src/Components/LoginPage/ForgetPassword.tsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { apis } from "../apis/constants/ApisService";
+import { useNavigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { getToken } from "../../features/authSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { message } from "antd";
+import axios from "axios";
+import { API_URL } from "../../Components/apis/constants/constant";
+
+type ForgetPasswordProps = {};
+
+export const ForgetPassword = ({}: ForgetPasswordProps) => {
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+  });
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log(inputs);
+    if (!inputs.email) {
+      message.error("All fields are required.");
+      return;
+    }
+    try {
+      const apiUrl = `${API_URL}/users/forgotPassword`; // Replace with your API endpoint URL
+
+      const response = await axios.post(apiUrl, inputs);
+
+      console.log(response.data);
+
+      if (response.status === 200) {
+        const { email, otp } = response.data;
+
+        // Store email and otp in cookies
+        document.cookie = `email=${email}; path=/`;
+        document.cookie = `otp=${otp}; path=/`;
+
+        navigate("/otpSection");
+      }
+    } catch (error) {
+      console.log(error);
+      message.error("Invalid Credentials");
+    }
+  };
+
+  return (
+    <main className="loginpage_main">
+      <div className="main_container container virtuosway-hr-login-page">
+        <form onSubmit={handleSubmit}>
+          <div className="main-logo-container d-flex align-items-center justify-content-center">
+            <div className="logo ">
+              <img src="/images/virtuos-logo.svg" alt="logo" />
+            </div>
+          </div>
+          <h2 className="sign-up-header">FORGOT PASSWORD? </h2>
+          <p className="sign-up-text">
+            To recover your password, enter your email address linked with your
+            slack account
+          </p>
+          <div className="form_container">
+            <div className="labels">
+              <label htmlFor="">Email:</label>
+            </div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your work email"
+              onChange={handleChange}
+            />
+            <p className="note-message">
+              Note: example@eeposit.com / example@virtuosway.com
+            </p>
+          </div>
+          <button type="submit" className="login-button">
+            Continue
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+};
--- src/Components/Routes/routes.tsx
+++ src/Components/Routes/routes.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from '../../pages/Dashboard/Dashboard';
-import { LoginPage } from '../LoginPage/login';
-import { PageNotFound } from '../PageNotFound/PageNotFound';
-import { ProtectedRoute } from './protectedRoute';
-import { Leave } from '../Leave/leave';
-import { Employee } from '../Employee';
-import Attendance from '../../pages/Attendance/Attendance';
-import Reports from '../../pages/Reports/Reports';
-import EmployeeAttendance from '../../pages/Attendance/EmployeeAttendance';
-import Shift from '../../pages/Shift/Shift';
-import ShiftDetails from '../../pages/Shift/ShiftDetails';
-import Holidays from '../../pages/Holidays/Holidays';
-import LeaveAllocation from '../../pages/LeaveAllocation/LeaveAllocation';
-import DeviceManager from '../../pages/DeviceManager/DeviceManager';
-import JobSummary from '../../pages/JobSumary/JobSummary';
-import Applicants from '../../pages/Applicants/Applicants';
-import LeaveDetails from '../../pages/LeaveAllocation/LeaveDetails';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "../../pages/Dashboard/Dashboard";
+import { LoginPage } from "../LoginPage/login";
+import { PageNotFound } from "../PageNotFound/PageNotFound";
+import { ProtectedRoute } from "./protectedRoute";
+import { Leave } from "../Leave/leave";
+import { Employee } from "../Employee";
+import Attendance from "../../pages/Attendance/Attendance";
+import Reports from "../../pages/Reports/Reports";
+import EmployeeAttendance from "../../pages/Attendance/EmployeeAttendance";
+import Shift from "../../pages/Shift/Shift";
+import ShiftDetails from "../../pages/Shift/ShiftDetails";
+import Holidays from "../../pages/Holidays/Holidays";
+import LeaveAllocation from "../../pages/LeaveAllocation/LeaveAllocation";
+import DeviceManager from "../../pages/DeviceManager/DeviceManager";
+import JobSummary from "../../pages/JobSumary/JobSummary";
+import Applicants from "../../pages/Applicants/Applicants";
+import LeaveDetails from "../../pages/LeaveAllocation/LeaveDetails";
+import { ForgetPassword } from "../LoginPage/ForgetPassword";
+import { OtpSection } from "../LoginPage/OtpSection";
+import { ChangePassword } from "../LoginPage/ChangePassword";
 
 type MainRoutesProps = {};
 
@@ -26,28 +29,31 @@ export const MainRoutes = ({}: MainRoutesProps) => {
     <React.Fragment>
       <Routes>
         <Route element={<ProtectedRoute />}>
-          <Route path='*' element={<PageNotFound />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/leave' element={<Leave />} />
-          <Route path='/employee' element={<Employee />} />
-          <Route path='/attendance' element={<Attendance />} />
-          <Route path='/reports' element={<Reports />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/leave" element={<Leave />} />
+          <Route path="/employee" element={<Employee />} />
+          <Route path="/attendance" element={<Attendance />} />
+          <Route path="/reports" element={<Reports />} />
           <Route
-            path='/attendance/:employeeId'
+            path="/attendance/:employeeId"
             element={<EmployeeAttendance />}
           />
-          <Route path='/shift' element={<Shift />} />
-          <Route path='/shift/:shiftId' element={<ShiftDetails />} />
-          <Route path='/holidays' element={<Holidays />} />
-          <Route path='/leave-allocation' element={<LeaveAllocation />} />
-          <Route path='/leave/:leaveId' element={<LeaveDetails />} />
+          <Route path="/shift" element={<Shift />} />
+          <Route path="/shift/:shiftId" element={<ShiftDetails />} />
+          <Route path="/holidays" element={<Holidays />} />
+          <Route path="/leave-allocation" element={<LeaveAllocation />} />
+          <Route path="/leave/:leaveId" element={<LeaveDetails />} />
 
-          <Route path='/device-manager' element={<DeviceManager />} />
-          <Route path='/job-summary' element={<JobSummary />} />
-          <Route path='/applicants' element={<Applicants />} />
+          <Route path="/device-manager" element={<DeviceManager />} />
+          <Route path="/job-summary" element={<JobSummary />} />
+          <Route path="/applicants" element={<Applicants />} />
         </Route>
 
-        <Route path='/' element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/forgetPassword" element={<ForgetPassword />} />
+        <Route path="/otpSection" element={<OtpSection />} />
+        <Route path="/ChangePassword" element={<ChangePassword />} />
       </Routes>
     </React.Fragment>
   );
