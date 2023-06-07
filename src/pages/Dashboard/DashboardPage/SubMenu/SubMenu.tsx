b89d375375ef7217347d9f434c315c0bb1e8e271
bikesh2023 <bchaudhary@eeposit.com>
2023-06-07 15:14:51 +0545

    fix: dashboard

--- src/pages/Dashboard/DashboardPage/SubMenu/SubMenu.tsx
+++ src/pages/Dashboard/DashboardPage/SubMenu/SubMenu.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './SubMenu.css';
-import { Button, Tabs } from 'antd';
+import { Button, Skeleton, Tabs } from 'antd';
 import { Graph } from '../Graph/Graph';
 import { DashboardEmployeeStatus } from '../DashboardEmployeeStatus/DashboardEmployeeStatus';
 import Analytics from '../Analytics/Analytics';
@@ -13,6 +13,7 @@ import { useAppSelector } from '../../../../hooks/useTypedSelector';
 import { RootState } from '../../../../store';
 import { verifyTokenStatus } from '../../../../redux/features/verifyTokenSlice';
 import { useDispatch } from 'react-redux';
+import { Spin } from 'antd';
 
 type SubMenuProps = {};
 
@@ -27,42 +28,56 @@ export const SubMenu = ({}: SubMenuProps) => {
   const userRole = tokenData?.role ? tokenData?.role : userData?.role;
   // const userRole = tokenData?.role;
 
+  if (!userRole) {
+    return (
+      <Spin
+        size='large'
+        style={{
+          position: 'absolute',
+          top: '50%',
+          right: '50%',
+        }}
+      />
+    );
+  }
   return (
     <div className='hr-dashboard-sub-menu'>
       <div className='row hr-dashboard-sub-menu-content'>
-        <Tabs defaultActiveKey='1'>
-          <Tabs.TabPane tab='Dashboard' key='1'>
-            {userRole === 'admin' ? (
-              <>
-                <div className='row'>
-                  <div className='col-lg-6 hr-dashboard-sub-menu-content-left'>
-                    <Graph />
-                    <DashboardEmployeeStatus />
+        <>
+          <Tabs defaultActiveKey='1'>
+            <Tabs.TabPane tab='Dashboard' key='1'>
+              {userRole === 'admin' ? (
+                <>
+                  <div className='row'>
+                    <div className='col-lg-6 hr-dashboard-sub-menu-content-left'>
+                      <Graph />
+                      <DashboardEmployeeStatus />
+                    </div>
+                    <div className='col-lg-6'>
+                      <Analytics />
+                      <AttendaceCount />
+                      {/* <EmployeeCountByDesignation /> */}
+                    </div>
                   </div>
-                  <div className='col-lg-6'>
-                    <Analytics />
-                    <AttendaceCount />
-                    {/* <EmployeeCountByDesignation /> */}
-                  </div>
-                </div>
-              </>
-            ) : (
-              <UserDashborad />
-            )}
-          </Tabs.TabPane>
-          {userRole === 'admin' && (
-            <Tabs.TabPane tab='Upcoming events' key='2'>
-              <div className='dashboard-upcoming-events'>
-                <div className='Upcoming col-8'>
-                  <UpcomingEvents />
-                </div>
-                <div className='announcement col-4'>
-                  <Announcement />
-                </div>
-              </div>
+                </>
+              ) : (
+                <UserDashborad />
+              )}
             </Tabs.TabPane>
-          )}
-        </Tabs>
+            {userRole === 'admin' && (
+              <Tabs.TabPane tab='Upcoming events' key='2'>
+                <div className='dashboard-upcoming-events'>
+                  <div className='Upcoming col-8'>
+                    <UpcomingEvents />
+                  </div>
+                  <div className='announcement col-4'>
+                    <Announcement />
+                  </div>
+                </div>
+              </Tabs.TabPane>
+            )}
+          </Tabs>
+        </>
       </div>
     </div>
   );
