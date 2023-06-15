f737e168c55b936af099f0e73236c2a83687c759
ajayeeposit <aranabhat@eposit.com>
2023-06-15 13:35:58 +0545

    fix:skeleton in userdashboard

--- src/Components/Profile/Projects/Projects.tsx
+++ src/Components/Profile/Projects/Projects.tsx
@@ -1,12 +1,12 @@
-import { Select } from 'antd';
-import { Space, Table, Tag, Input } from 'antd';
-import NepaliDate from 'nepali-date-converter';
-import type { ColumnsType } from 'antd/es/table';
-import { AiOutlineSearch } from 'react-icons/ai';
-import './projects.css';
-import { useEffect, useState } from 'react';
-import { useGetProjectTeamQuery } from '../../../redux/features/projectTeam.slice';
-import Spinner from '../../Spinner/Spinner';
+import { Select } from "antd";
+import { Space, Table, Tag, Input } from "antd";
+import NepaliDate from "nepali-date-converter";
+import type { ColumnsType } from "antd/es/table";
+import { AiOutlineSearch } from "react-icons/ai";
+import "./projects.css";
+import { useEffect, useState } from "react";
+import { useGetProjectTeamQuery } from "../../../redux/features/projectTeam.slice";
+import Spinner from "../../Spinner/Spinner";
 
 interface DataType {
   key: string;
@@ -26,25 +26,30 @@ type Team = {
 
 const columns: ColumnsType<DataType | Team> = [
   {
-    title: 'TEAM MEMBERS',
-    dataIndex: 'teamMember',
-    key: 'teamMember',
+    title: "TEAM MEMBERS",
+    dataIndex: "teamMember",
+    key: "teamMember",
     render: ({ name, email }) => (
-      <div>
+      <div style={{ height: "60px" }}>
         <p>{name}</p>
         <p>{email}</p>
       </div>
     ),
   },
   {
-    title: 'DATE ADDED',
-    dataIndex: 'date',
-    key: 'date',
-    render: (date) => new NepaliDate('2075-03-05').format('MMMM DD, YYYY'),
+    title: "DATE ADDED",
+    dataIndex: "date",
+    key: "date",
+    render: (date) => new NepaliDate("2075-03-05").format("MMMM DD, YYYY"),
   },
 ];
 
-const Projects = ({ projectTeam = 'HRMS' }: PropTypes) => {
+// const CustomTable = styled(Table)`
+// '& .antd-table-head`: {
+//   background: red;
+// }
+
+const Projects = ({ projectTeam = "HRMS" }: PropTypes) => {
   const [team, setTeam] = useState<Team[]>();
   const [teamCache, setTeamCache] = useState<Team[]>();
   const { data, error, isLoading } = useGetProjectTeamQuery(projectTeam);
@@ -81,41 +86,43 @@ const Projects = ({ projectTeam = 'HRMS' }: PropTypes) => {
   };
 
   return (
-    <div className='project-container'>
+    <div className="project-container">
       <p>Assigned Projects</p>
       {isLoading ? (
         <Spinner />
       ) : (
         <>
-          <div className='project-input-container'>
+          <div className="project-input-container">
             <Select
               showSearch
-              size='large'
-              placeholder='Select a project'
-              optionFilterProp='children'
+              size="large"
+              placeholder="Select a project"
+              optionFilterProp="children"
               onChange={onChange}
               onSearch={onSearch}
               style={{ width: 286 }}
               filterOption={(input, option) =>
-                (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+                (option?.label ?? "")
+                  .toLowerCase()
+                  .includes(input.toLowerCase())
               }
               defaultValue={projectTeam}
               options={[{ value: projectTeam, label: projectTeam }]}
             />
 
             <Input
-              placeholder='Search'
+              placeholder="Search"
               style={{ width: 200 }}
-              size='large'
+              size="large"
               onChange={onInputSearch}
               suffix={<AiOutlineSearch />}
             />
           </div>
 
-          <div className='project-table-container'>
+          <div className="project-table-container">
             <Table
               columns={columns}
-              pagination={{ position: ['bottomCenter'], pageSize: 5 }}
+              pagination={{ position: ["bottomCenter"], pageSize: 5 }}
               dataSource={team}
             />
           </div>
