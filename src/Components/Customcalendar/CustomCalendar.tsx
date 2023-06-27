9dfdf39cd27ea56b9855cdfbb949519c65293d84
biwashpra <biwashpra3883@gmail.com>
2023-06-27 13:41:25 +0545

    Chate ko kamal in Calender

--- src/Components/Customcalendar/CustomCalendar.tsx
+++ src/Components/Customcalendar/CustomCalendar.tsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
-import { useAppSelector } from '../../hooks/useTypedSelector';
-import { EmployeeStats } from '../../pages/Attendance/Attendance';
-import { IYear } from '../../pages/Attendance/EmployeeAttendance';
-import { getEmployeeData } from '../../redux/features/SingleAttendanceSlice';
+import { useAppSelector } from "../../hooks/useTypedSelector";
+import { EmployeeStats } from "../../pages/Attendance/Attendance";
+import { IYear } from "../../pages/Attendance/EmployeeAttendance";
+import { getEmployeeData } from "../../redux/features/SingleAttendanceSlice";
 import {
   daysOfWeek,
   monthNames,
   NepaliMonthDays,
   nepaliMonthDays,
-} from '../../utils/Constants';
+} from "../../utils/Constants";
 
-import './Customcalendar.css';
-import { todayInBs } from './GetTodaysDate';
+import "./Customcalendar.css";
+import { todayInBs } from "./GetTodaysDate";
 
 function Calendar({ month, year }: { month: number; year: IYear }) {
   let yearNumber: keyof NepaliMonthDays = year.year.toString();
@@ -113,113 +113,120 @@ function Calendar({ month, year }: { month: number; year: IYear }) {
   );
 
   const checkStatus = (morningStatus: string, eveningStatus: string) => {
-    morningStatus === 'early-in' && eveningStatus === 'late-out'
+    morningStatus === "early-in" && eveningStatus === "late-out"
       ? setEarlyIn(true)
       : setEarlyIn(false);
     return earlyIn;
   };
 
+  // Filter employee attendance data for the current month
+  const filteredAttendance = employee?.result?.filter((item: any) => {
+    const attendanceDate = new Date(item.attendanceByDate.date);
+    return (
+      attendanceDate.getFullYear() === year.year &&
+      attendanceDate.getMonth() === month
+    );
+  });
+
   // Render the calendar
   return (
-    <div className='calendar-wrapper'>
-      <h2 className='calendar-title'>
+    <div className="calendar-wrapper">
+      <h2 className="calendar-title">
         {year.year} {monthNames[month]}
       </h2>
 
-      <table className='calendar-container'>
-        <thead className='calendar-header'>
+      <table className="calendar-container">
+        <thead className="calendar-header">
           <tr>
             {daysOfWeek.map((day) => (
-              <th key={day} className='calendar-head'>
+              <th key={day} className="calendar-head">
                 {day}
               </th>
             ))}
           </tr>
         </thead>
-        <tbody className='table-body-container'>
+        <tbody className="table-body-container">
           {weeks.map((week: any, i: number) => (
-            <tr key={i} className='table-row-container'>
+            <tr key={i} className="table-row-container">
               {week.map((day: any, j: number) => (
-                <>
-                  <td key={`${i}-${j}`} className='table-data-container'>
-                    {day}
-                    {employee?.result?.map((item: any, index: number) => {
-                      const dayDate = new Date(
-                        item.attendanceByDate.date
-                      ).getDate();
-
-                      if (dayDate === day) {
-                        return item?.attendanceByDate?.holiday ? (
-                          <div className='status-container'>
-                            <EmployeeStats
-                              backgroundColor='transparent'
-                              color='#9747FF'
-                              status='Holiday'
-                              classNames='holiday-container'
-                            />
-                          </div>
-                        ) : item?.attendanceByDate?.absent ? (
-                          <div className='status-container'>
-                            <EmployeeStats
-                              backgroundColor='#'
-                              color='#BB2124'
-                              status='Absent'
-                              classNames='holiday-container'
-                            />
-                          </div>
-                        ) : (
-                          <div className='status-container'>
-                            <EmployeeStats
-                              backgroundColor={
-                                item?.attendanceByDate?.morningStatus?.toLowerCase() ===
-                                'timely in'
-                                  ? '#F4FBF5'
-                                  : item?.attendanceByDate?.morningStatus?.toLowerCase() ===
-                                    'late in'
-                                  ? '#FBF4F4'
-                                  : ''
-                              }
-                              color={
-                                item?.attendanceByDate?.morningStatus?.toLowerCase() ===
-                                'timely in'
-                                  ? '#22BB33'
-                                  : item?.attendanceByDate?.morningStatus?.toLowerCase() ===
-                                    'late in'
-                                  ? '#BB2124'
-                                  : ''
-                              }
-                              status={item.attendanceByDate.entryTime}
-                              numberOfEmployee={item.numberOfEmployee}
-                            />
-                            -{' '}
-                            <EmployeeStats
-                              backgroundColor={
-                                item?.attendanceByDate?.eveningStatus?.toLowerCase() ===
-                                'timely out'
-                                  ? '#F4FBF5'
-                                  : item?.attendanceByDate?.eveningStatus?.toLowerCase() ===
-                                    'early out'
-                                  ? '#FEFBF6'
-                                  : ''
-                              }
-                              color={
-                                item?.attendanceByDate?.eveningStatus?.toLowerCase() ===
-                                'timely out'
-                                  ? '#22BB33'
-                                  : item?.attendanceByDate?.eveningStatus?.toLowerCase() ===
-                                    'early out'
-                                  ? '#F0AD4E'
-                                  : ''
-                              }
-                              status={item.attendanceByDate.exitTime}
-                              numberOfEmployee={item.numberOfEmployee}
-                            />
-                          </div>
-                        );
-                      }
-                    })}
-                  </td>
-                </>
+                <td key={`${i}-${j}`} className="table-data-container">
+                  {day}
+                  {filteredAttendance?.map((item: any, index: number) => {
+                    const dayDate = new Date(
+                      item.attendanceByDate.date
+                    ).getDate();
+                    if (dayDate === day) {
+                      // console.log(item?.attendanceByDate, "HAHA");
+                      return item?.attendanceByDate?.holiday ? (
+                        <div className="status-container">
+                          <EmployeeStats
+                            backgroundColor="transparent"
+                            color="#9747FF"
+                            status="Holiday"
+                            classNames="holiday-container"
+                          />
+                        </div>
+                      ) : item?.attendanceByDate?.absent ? (
+                        <div className="status-container">
+                          <EmployeeStats
+                            backgroundColor="#"
+                            color="#BB2124"
+                            status="Absent"
+                            classNames="holiday-container"
+                          />
+                        </div>
+                      ) : (
+                        <div className="status-container">
+                          <EmployeeStats
+                            backgroundColor={
+                              item?.attendanceByDate?.morningStatus?.toLowerCase() ===
+                              "timely in"
+                                ? "#F4FBF5"
+                                : item?.attendanceByDate?.morningStatus?.toLowerCase() ===
+                                  "late in"
+                                ? "#FBF4F4"
+                                : ""
+                            }
+                            color={
+                              item?.attendanceByDate?.morningStatus?.toLowerCase() ===
+                              "timely in"
+                                ? "#22BB33"
+                                : item?.attendanceByDate?.morningStatus?.toLowerCase() ===
+                                  "late in"
+                                ? "#BB2124"
+                                : ""
+                            }
+                            status={item.attendanceByDate.entryTime}
+                            numberOfEmployee={item.numberOfEmployee}
+                          />
+                          -{" "}
+                          <EmployeeStats
+                            backgroundColor={
+                              item?.attendanceByDate?.eveningStatus?.toLowerCase() ===
+                              "timely out"
+                                ? "#F4FBF5"
+                                : item?.attendanceByDate?.eveningStatus?.toLowerCase() ===
+                                  "early out"
+                                ? "#FEFBF6"
+                                : ""
+                            }
+                            color={
+                              item?.attendanceByDate?.eveningStatus?.toLowerCase() ===
+                              "timely out"
+                                ? "#22BB33"
+                                : item?.attendanceByDate?.eveningStatus?.toLowerCase() ===
+                                  "early out"
+                                ? "#F0AD4E"
+                                : ""
+                            }
+                            status={item.attendanceByDate.exitTime}
+                            numberOfEmployee={item.numberOfEmployee}
+                          />
+                        </div>
+                      );
+                    }
+                  })}
+                </td>
               ))}
             </tr>
           ))}
@@ -228,4 +235,5 @@ function Calendar({ month, year }: { month: number; year: IYear }) {
     </div>
   );
 }
+
 export default Calendar;
