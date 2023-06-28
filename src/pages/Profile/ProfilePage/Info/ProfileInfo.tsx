1cfcf28717c66012128405422225f3a3d8b4fcac
bikesh2023 <bchaudhary@eeposit.com>
2023-06-28 15:54:38 +0545

    fix; view profile

--- src/pages/Profile/ProfilePage/Info/ProfileInfo.tsx
+++ src/pages/Profile/ProfilePage/Info/ProfileInfo.tsx
@@ -1,16 +1,14 @@
 import ProfileForm from '../../../../Components/Profile/ProfileForm';
 import { Col, Row } from 'antd';
-import { useAppSelector } from '../../../../hooks/useTypedSelector';
 import { useGetUserProfileQuery } from '../../../../redux/features/profileSlice';
-
 import Spinner from '../../../../Components/Spinner/Spinner';
 import Projects from '../../../../Components/Profile/Projects/Projects';
 import './profileInfo.css';
+import { useTokenData } from '../../../../hooks/userTokenData';
 
 const ProfileInfo = () => {
-  const { tokenData } = useAppSelector((state: any) => state.verifyTokenSlice);
-  const { data, isLoading } = useGetUserProfileQuery(tokenData.userSn || '');
-
+  const { userSn } = useTokenData();
+  const { data, isLoading } = useGetUserProfileQuery(userSn);
 
   return (
     <>
@@ -20,11 +18,7 @@ const ProfileInfo = () => {
         <>
           <Row>
             <Col span={16}>
-              <ProfileForm
-                isDisable={false}
-                employeeId={tokenData.userSn}
-                defaultValue={{ ...data.employee }}
-              />
+              <ProfileForm isDisable={false} employeeId={userSn} defaultValue={{ ...data?.employee }} />
             </Col>
 
             <Col span={8}>
